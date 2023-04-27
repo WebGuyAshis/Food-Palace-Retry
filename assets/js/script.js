57b2ab58377c866c488007cbadb65850bcd5dcf6
Ashis <developer.ashis99@gmail.com>
2023-04-27 19:12:38 +0530

    Fixed Vh issues in Phone Devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -361,3 +361,12 @@ function addToFav(id) {
     // }
   })
 }
+
+
+// To handle the vh issue in mobile phones 
+const setVh = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+setVh();
+window.addEventListener('resize', setVh);
